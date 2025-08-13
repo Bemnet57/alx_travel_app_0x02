6748f102b2363fb4a4fc712df14e0c7b02269a96
Bemnet57 <bemnetasseged57@gmail.com>
2025-08-13 19:32:15 +0300

    created payment model

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Payment(models.Model):
+    booking_reference = models.CharField(max_length=100)
+    transaction_id = models.CharField(max_length=100, blank=True, null=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20, default='Pending')  # Pending, Completed, Failed
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.booking_reference} - {self.status}"
